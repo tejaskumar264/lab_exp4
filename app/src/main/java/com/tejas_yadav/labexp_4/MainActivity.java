13e0f026f2da4f587ae592bc023a560c43f9ca10
TEJAS KUMAR <tejaskumaryadav264@gmail.com>
2021-12-23 20:09:23 +0530

    lab_exp4_tejas

---
+++ app/src/main/java/com/tejas_yadav/labexp_4/MainActivity.java
@@ -0,0 +1,31 @@
+package com.tejas_yadav.labexp_4;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startBackGroundProcessButtonClick(View view){
+
+        Intent intent = new Intent(this,myBackgroundProcess.class);
+        intent.setAction("BackgroundProcess");
+
+        //Set the repeated Task
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 10, pendingIntent);
+
+        finish();
+    }
+}
